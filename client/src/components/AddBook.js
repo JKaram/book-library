907bc7796620d897905f15744c9ea2f5e1536735
Jamie <Jamiekaram16@gmail.com>
2020-03-29 22:05:19 -0400

    add book to mongo

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,33 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
+import { flowRight as compose } from 'lodash';
 
-import { getAuthorsQuery } from '../queries/queries'
+
+import { getAuthorsQuery, addBookMutation } from '../queries/queries'
 
 
-function AddBook({ data }) {
+function AddBook( props ) {
+    const [form, setForm] = useState({
+        name: '',
+        genre: '',
+        authorId: ''
+    })
+    console.log(props, 'Data')
     function displayAuthors() {
-        if (data.loading) {
+        if (props.getAuthorsQuery.loading) {
             return (<option disabled>Loading authors</option>);
         } else {
-            return data.authors.map(author => {
+            return props.getAuthorsQuery.authors.map(author => {
                 return (<option key={author.id} value={author.id}>{author.name}</option>);
             });
         }
     }
 
+    function submitForm(e) {
+        e.preventDefault()
+        props.addBookMutation({
+            variables: {
+                name: form.name,
+                genre: form.genre,
+                authorId: form.authorId
+            }
+        })
+    }
+
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={submitForm}>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text" />
+                <input type="text" onChange={e => setForm({ ...form, name: e.target.value })} />
             </div>
             <div className="field">
                 <label>Genre:</label>
-                <input type="text" />
+                <input type="text" onChange={e => setForm({ ...form, genre: e.target.value })} />
             </div>
             <div className="field">
                 <label>Author:</label>
-                <select>
+                <select onChange={e => setForm({ ...form, authorId: e.target.value })}>
                     <option>Select author</option>
                     {displayAuthors()}
                 </select>
@@ -38,4 +57,7 @@ function AddBook({ data }) {
     );
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default compose(
+    graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+    graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
