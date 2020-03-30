5bdb84b30452bf98c688bcee740b1b960b91cf76
Jamie <Jamiekaram16@gmail.com>
2020-03-29 22:54:23 -0400

    click book for details

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql } from 'react-apollo';
 
+import BookDetails from './BookDetails'
 import { getBooksQuery } from '../queries/queries'
 
 
 function BookList({ data }) {
+    const [showBook, setShowBook] = useState(null);
     function displayBooks() {
         if (data.loading) {
             return (
@@ -13,17 +15,18 @@ function BookList({ data }) {
         } else {
             return data.books.map(book => {
                 return (
-                    <li key={book.id}>{book.name}</li>
+                    <li key={book.id} onClick={e => {setShowBook(book.id)}}>{book.name}</li>
                 )
             })
         }
     }
-
+    console.log(data, 'DATA')
     return (
         <div className="App">
             <ul id="book-list">
                 {displayBooks()}
             </ul>
+            <BookDetails bookId={showBook}/>
         </div>
     );
 }
